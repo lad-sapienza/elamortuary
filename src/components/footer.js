fcf5c08729623141d221dd1f59cefc90ff43be3b
YasminaElam <167799278+YasminaElam@users.noreply.github.com>
2024-04-24 00:58:16 +1000

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -24,11 +24,8 @@ const FooterSection = () => {
             <p className="p-s-3">
               The <strong>ELAMortuary</strong> database is the outcome of the
               project 
-              <em>
-              Villages to Empire: 4,000 Years of Death and Society in Elam
-              (4500-525 BCE)
-              </em> 
-              carried out by the project researcher, Yasmina Wicks, in the frame
+              <em>Villages to Empire: 4,000 Years of Death and Society in Elam (4500-525 BCE)</em> 
+               carried out by the project researcher, Yasmina Wicks, in the frame
               of a {" "}
               <a
                 href="https://cordis.europa.eu/project/id/892581"
